74cb8d6281132a1ad889e9bb6ca20813e4afd16f
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-10-13 16:24:16 +0700

    autocorrelation function with window size equal to 1024

--- PitchEstimation/main.py
+++ PitchEstimation/main.py
@@ -22,5 +22,30 @@ def auto_correlation_function(window):
 
 if __name__ == "__main__":
     sample_rate, signal = read_signal_from_file(
-        "PitchEstimation/TinHieuHuanLuyen/phone_F2.wav"
+        "PitchEstimation/TinHieuHuanLuyen/phone_M2.wav"
     )
+    # window size
+    window_size = 1024
+    # normalize signal
+    normalized_signal = signal / np.amax(signal)
+    x = np.arange(0, signal.shape[0])
+    plt.figure()
+    plt.plot(x, normalized_signal)
+    plt.xlabel("Time(s)")
+    plt.ylabel("Amplitude")
+
+    auto_correlation_points = []
+    start = 0
+    end = 1024
+    while end < signal.shape[0]:
+        temp_window = np.float64(signal[start:end])
+        for value in auto_correlation_function(temp_window):
+            auto_correlation_points.append(value)
+        start += 1024
+        end += 1024
+    for value in auto_correlation_function(np.float64(signal[start : signal.shape[0]])):
+        auto_correlation_points.append(value)
+    plt.figure()
+    plt.plot(x, auto_correlation_points)
+
+    plt.show()
