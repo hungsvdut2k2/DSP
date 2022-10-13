cacdbab29aa00a006cdef0c6f085f50f58cc1631
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-10-13 15:09:26 +0700

    implement autocorrelation function

--- PitchEstimation/main.py
+++ PitchEstimation/main.py
@@ -1,3 +1,4 @@
+from curses import window
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
@@ -8,24 +9,18 @@ def read_signal_from_file(path):
     return sample_rate, signal
 
 
+def auto_correlation_function(window):
+    window_size = len(window)
+    result = []
+    for i in range(window_size):
+        sigma = 0
+        for j in range(window_size - i):
+            sigma += window[j] * window[j + i]
+        result.append(sigma / window_size)
+    return result
+
+
 if __name__ == "__main__":
     sample_rate, signal = read_signal_from_file(
-        "/home/viethung/DSP/TinHieuHuanLuyen/phone_F2.wav"
+        "PitchEstimation/TinHieuHuanLuyen/phone_F2.wav"
     )
-    # plot input signal
-    x = np.arange(0, signal.shape[0])
-    plt.figure()
-    plt.plot(x, signal)
-    plt.xlabel("Time(s)")
-    plt.ylabel("Amplitude")
-    plt.xticks(np.arange(0, 6) * sample_rate, labels=[0, 1, 2, 3, 4, 5])
-
-    normalized_signal = signal / np.amax(signal)
-    x = np.arange(0, signal.shape[0])
-    plt.figure()
-    plt.plot(x, normalized_signal)
-    plt.xlabel("Time(s)")
-    plt.ylabel("Amplitude")
-    plt.xticks(np.arange(0, 6) * sample_rate, labels=[0, 1, 2, 3, 4, 5])
-
-    plt.show()
